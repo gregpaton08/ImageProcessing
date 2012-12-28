bef7d9f44d41a25fcf96187f20005d7b60eb854c
Greg <gregpaton08@gmail.com>
2012-12-28 15:18:02 -0500

    [mod] resize function now working

--- test.cpp
+++ test.cpp
@@ -10,8 +10,9 @@
 
 #include "bitmap_image.hpp"
 
-void upsize(const unsigned char *data, bitmap_image &image, int width, int height, int bpp);
-void downsize(const unsigned char *data, bitmap_image &image, int width, int height, int bpp);
+#define round(x) (int)(x+0.5)
+
+void resize(const unsigned char *data, bitmap_image &image, int width, int height, int w, int h, int bpp);
 
 int main() 
 {
@@ -30,79 +31,33 @@ int main()
     // get bytes per pixel
     bpp = image.bytes_per_pixel();
     
-//    for (int i = 0; i < width; ++i) {
-//        for (int j = 0; j < height; ++j) {
-//            // access pixels using
-//            // image.get_pixel(i, j, r, g, b);
-//            // or 
-//            r = data[(j * width * bpp) + (i * bpp) + 2];
-//            g = data[(j * width * bpp) + (i * bpp) + 1];
-//            b = data[(j * width * bpp) + (i * bpp) + 0];
-//            //image.set_pixel(i, j, r, g, b);
-//        }
-//    }
-//    image.save_image("saved.bmp");
-    
-    bitmap_image upimg(1024, 1024);
-    upsize(data, upimg, width, height, bpp);
-    upimg.save_image("up.bmp");
-    bitmap_image downimg(256, 256);
-    downsize(data, downimg, width, height, bpp);
-    downimg.save_image("down.bmp");
+    bitmap_image img(1024, 1024);
+    resize(data, img, width, height, img.width(), img.height(), bpp);
+    img.save_image("new.bmp");
     
     return 0;
 }
 
-// type : specifies upsize/downsize
-//        1 - upsize
-//        2 - downsize
-bool resize(const unsigned char *data, unsigned char *res, int width, int height, int type) {
-    if (type == 1) {
-        
-    }
-    else if (type == 2) {
-        
-    }
-    else
-        return false;
-    
-    return true;
-}
 
-void down_size(const unsigned char *data, bitmap_image &image, int width, int height, int w, int h, int bpp) {
+void resize(const unsigned char *data, bitmap_image &image, int width, int height, int w, int h, int bpp) {
+    // determine scale factors
     double sf_w = (double)width / (double)w;
     double sf_h = (double)height / (double)h;
-}
-
-void downsize(const unsigned char *data, bitmap_image &image, int width, int height, int bpp) {
-    
     unsigned char r, g, b;
+    int _i, _j;
     
-    for (int i = 0; i < width / 2; ++ i) {
-        for (int j = 0; j < height / 2; ++j) {
-            r = data[(j * 2 * width * bpp) + (i * 2 * bpp) + 2];
-            g = data[(j * 2 * width * bpp) + (i * 2 * bpp) + 1];
-            b = data[(j * 2 * width * bpp) + (i * 2 * bpp) + 0];
+    for (int i = 0; i < w; ++i) {
+        for (int j = 0; j < h; ++j) {
+            // find nearest neighbor
+            _i = round(i*sf_w);
+            _j = round(j*sf_h);
             
+            r = data[(_j * width * bpp) + (_i * bpp) + 2];
+            g = data[(_j * width * bpp) + (_i * bpp) + 1];
+            b = data[(_j * width * bpp) + (_i * bpp) + 0];
+            
+            // write to new image
             image.set_pixel(i, j, r, g, b);
-        }
-    }
-}
-
-void upsize(const unsigned char *data, bitmap_image &image, int width, int height, int bpp) {
-    
-    unsigned char r, g, b;
-    
-    for (int i = 0; i < width; ++i) {
-        for (int j = 0; j < height; ++j) {
-            r = data[(j * width * bpp) + (i * bpp) + 2];
-            g = data[(j * width * bpp) + (i * bpp) + 1];
-            b = data[(j * width * bpp) + (i * bpp) + 0];
-            
-            image.set_pixel(i*2,   j*2,   r, g, b);
-            image.set_pixel(i*2,   j*2+1, r, g, b);
-            image.set_pixel(i*2+1, j*2,   r, g, b);
-            image.set_pixel(i*2+1, j*2+1, r, g, b);
         }
     }
 }
